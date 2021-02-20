8a98ef94542d16531f4e870ea4fc7cb7c3704036
Adam Políček <xpolic05@vutbr.cz>
2021-02-20 21:00:03 +0100

    Main structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Festivals_overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
